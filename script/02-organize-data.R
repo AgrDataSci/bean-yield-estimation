89281ca0e5b208e9b1ded2132650955b7af76dc1
kauedesousa <kaue.desousa@inn.no>
2023-01-10 14:37:49 +0100

    organize-names-in-data

--- script/02-organize-data.R
+++ script/02-organize-data.R
@@ -3,70 +3,30 @@
 
 # load packages
 library("ClimMobTools")
+library("readxl")
 library("gosset")
 library("janitor")
 
+list.files("data", full.names = TRUE)
 
 load("processing/trial-data.rda")
 
-# keep only the projects with data on it
-keep <- lapply(cmdata, function(x){
-  length(x[["data"]]) > 1
-})
+# check names of sheets
+filename = "data/Yield determination survey.xlsx"
 
-keep <- unlist(keep)
+excel_sheets(filename)
 
-sum(keep) / length(keep)
+# data file
+dat = read_excel(filename, sheet = 2, na = "NA")
 
-# add the logic vector to the data with project info
-projects$keep <- keep
+# descriptors and labels
+descrip = read_excel(filename, sheet = 3, na = "NA")
 
-cmdata <- cmdata[keep]
-
-# put the data into data.frame format
-dat <- lapply(cmdata, function(x){
-  x <- as.data.frame(x, 
-                     tidynames = TRUE,
-                     pivot.wider = TRUE)
-  
-  names(x) <- make_clean_names(names(x))
-  
-  x
-  
-})
-
-# get unique col names across the data.frames
-uniquenames <- lapply(dat, function(x){
-  names(x)
-})
-
-uniquenames <- unique(unlist(uniquenames))
-
-
-# fix some codes that diverged over time
-dat <- lapply(dat, function(x){
-  names(x)[names(x) == "registration_gender1"] <- "registration_gender"
-  names(x)[names(x) == "registration_biogender"] <- "registration_gender"
-  names(x)[names(x) == "registration_bioage"] <- "registration_age"
-  names(x)[names(x) == "registration_kebele"] <- "registration_village"
-  names(x)[names(x) == "registration_location"] <- "registration_village"
-
-  x
-})
-
-# put the data together 
-dat <- rowbind(dat)
-
-# create an id combining package id and project code 
-dat$block_id <- paste(dat$package_project_name, dat$id, sep = "-")
-
-dat <- dat[, union("block_id", names(dat))]
+# make clean names
+names(dat) = make_clean_names(names(dat))
 
 
 
-# Coordinates
-# get the coordinates as an independent data.frame
-keep <- grepl("_longitude|_latitude", names(dat))
 
-lonlat <- dat[, keep]
+
 
